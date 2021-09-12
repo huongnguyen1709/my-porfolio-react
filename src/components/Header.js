649118cf6fa8e4024292aac73e54954dc1175b87
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2021-09-12 15:12:44 +0300

    add NavBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,22 +5,13 @@ const Header = () => {
     return (
         <header id="header">
         <div className="header">
-            <nav className="navigation">
-                <ul className="nav-list">
-                    <li><a href="#header" className="nav-link">Home</a></li>
-                    <li><a href="#about" className="nav-link">About</a></li>
-                    <li><a href="#project" className="nav-link">Project</a></li>
-                    <li><a href="#contact" className="nav-link">Contact</a></li>
-                </ul>
-            </nav>
-
             <NavBar />
 
             <div className="header-text-box">
                 <div className="border">
                     <div className="inner">
                         <h1 className="header-text heading-main">Huong Nguyen</h1>
-                        <h5 className="header-text heading-sub">UI Designer & <span className="heading-sub-2">Front-end Web
+                        <h5 className="header-text heading-sub">UI Designer & <span className="heading-sub-2">Full-Stack Web
                                 Developer</span></h5>
                         <a href="#about" className="btn-click">Click me !</a>
                     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,15 @@ import React from 'react';
 const NavBar = () => {
     return (
        <div>
-           <nav className="ham-btn">
+            <nav className="navigation">
+                <ul className="nav-list">
+                    <li><a href="#header" className="nav-link">Home</a></li>
+                    <li><a href="#about" className="nav-link">About</a></li>
+                    <li><a href="#project" className="nav-link">Project</a></li>
+                    <li><a href="#contact" className="nav-link">Contact</a></li>
+                </ul>
+            </nav>
+           {/* <nav className="ham-btn">
                 <span></span>
                 <span></span>
                 <span></span>
@@ -13,7 +21,7 @@ const NavBar = () => {
                 <a href="#about" className="nav-link">About</a>
                 <a href="#project" className="nav-link">Project</a>
                 <a href="#contact" className="nav-link">Contact</a>
-            </div>
+            </div> */}
        </div>
     );
 };
