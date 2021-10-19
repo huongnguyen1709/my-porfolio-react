ddeefd26386b70e759474761c7c612fad4a6820c
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2021-10-19 16:04:21 +0300

    set responsive for computer

--- src/components/about/CardSlider.js
+++ src/components/about/CardSlider.js
@@ -5,12 +5,12 @@ import SkillsCard from './SkillsCard';
 const CardSlider = ({skills}) => {
     const slideLeft = () => {
         var slider = document.getElementById('slider')
-        slider.scrollLeft = slider.scrollLeft - 500
+        slider.scrollLeft = slider.scrollLeft - 900
     }
 
     const slideRight = () => {
         var slider = document.getElementById('slider')
-        slider.scrollLeft = slider.scrollLeft + 700
+        slider.scrollLeft = slider.scrollLeft + 900
     }
 
     return (
