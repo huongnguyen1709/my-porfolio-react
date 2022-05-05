621c2df394a1bf605260ac1ce985b3c256e17817
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2022-05-05 13:39:08 +0300

    modify About page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -31,16 +31,17 @@ const About = () => {
         <div className='about'>
             <h2 className="heading-about">About Me</h2>
             <p className="introduce">Hi, I'm Huong. 
-            I have a passion in designing and building website, playing with animation and UI effects in front-end web development. I am self-motivation and optimistic that promote myself to overcome the problem without
-giving up. I am curious and constantly learning to keep myself updated. My hobbies are reading, traveling, and talking with people. <br></br>
+            I have a passion in coding, building websites & applications. I am self-motivation and optimistic that promote myself to overcome the problem without
+giving up. I am curious and constantly learning to keep myself updated. My hobbies are reading, traveling, and communicating with people. <br></br>
             <br></br>  
-            Feel free to check out my skills & projects !
+            Feel free to check out my skills & projects ! <br></br> 
             <br></br>  
             <u>Education</u> : <br></br>  
-            &nbsp; • &nbsp; Bachelor of Engineering of Information & Communication Technology at Turku University of Applied Sciences <br></br> 
+            &nbsp; • &nbsp; Degree: Bachelor of Engineering of Information & Communication Technology <br></br> 
+            &nbsp; • &nbsp; School: Turku University of Applied Sciences <br></br> 
             <br></br>  
             <u>Achievements</u> : <br></br>  
-            &nbsp; &nbsp; • &nbsp; Full Stack Open 2021, University of Helsinki & Houston Inc. <br></br>  
+            &nbsp; • &nbsp; Full Stack Open 2021, University of Helsinki & Houston Inc. <br></br>  
             &nbsp; &nbsp; • &nbsp; GraphQL, Full Stack Open 2021, University of Helsinki & Houston Inc.  <br></br> 
             &nbsp; &nbsp; • &nbsp; TypeScript, Full Stack Open 2021, University of Helsinki & Houston Inc.  <br></br> 
             &nbsp; &nbsp; • &nbsp; CI/CD, Full Stack Open 2021, University of Helsinki & Houston Inc.  <br></br> 
